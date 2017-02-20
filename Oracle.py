a3ed77c3351fd741807985079a1edb8443644a56
Kirill Kuklin <kk90@mail.com>
2017-02-20 10:12:08 +0300

    get_logs has been introduced. results come back with print option. to be fixed: return with return

--- Oracle.py
+++ Oracle.py
@@ -1,35 +1,20 @@
 import paramiko
 import time
-import re
-
-
-def disable_paging(remote_conn):
-
-    remote_conn.send("terminal length 0\n")
-    time.sleep(1)
-
-    # Clear the buffer on the screen
-    output = remote_conn.recv(1000)
-
-    return output
-
 
 if __name__ == '__main__':
-    # VARIABLES THAT NEED CHANGED
-    ip = '172.17.13.89'
-    username = 'oracle'
-    password = '642362kkk'
-    # ip = '172.17.12.58'
+    # ip = '172.17.13.89'
     # username = 'oracle'
-    # password = 'Veeam123'
+    # password = '642362kkk'
+    ip = '172.17.12.58'
+    username = 'oracle'
+    password = 'Veeam123'
     res = {}
 
     # Create instance of SSHClient object
     remote_conn_pre = paramiko.SSHClient()
 
     # Automatically add untrusted hosts (make sure okay for security policy in your environment)
-    remote_conn_pre.set_missing_host_key_policy(
-        paramiko.AutoAddPolicy())
+    remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     # initiate SSH connection
     remote_conn_pre.connect(ip, username=username, password=password, look_for_keys=False, allow_agent=False)
@@ -37,7 +22,9 @@ if __name__ == '__main__':
     # Use invoke_shell to establish an 'interactive session'
     remote_conn = remote_conn_pre.invoke_shell()
 
-def get_user(remote_conn):
+
+def get_user(remote_conn):  # remote_conn type is <class 'paramiko.channel.Channel'>
+
     remote_conn.send("sqlplus / as sysdba\n")
     time.sleep(1)
     remote_conn.send("show user\n")
@@ -51,5 +38,24 @@ def get_user(remote_conn):
             res[i.lstrip('SQL>').split()[0]] = i.lstrip('SQL>').split()[2][1:-1]
     return res
 
-print(get_user(remote_conn))
+# print(get_user(remote_conn))
 
+
+def get_logs(remote_conn):  # remote_conn type is <class 'paramiko.channel.Channel'>
+    remote_conn.send("sqlplus / as sysdba\n")
+    time.sleep(1)
+    remote_conn.send("set linesize 3000\nselect *from v$log;\n")
+    time.sleep(2)
+    output = remote_conn.recv(10000)
+    # return output
+    sqlplus = output.decode("utf-8")
+    lines = sqlplus.splitlines(True)
+    rng = range (0, len(lines))
+    newrng = range (0, 1)
+    for i in rng:
+        # print(i, lines[i].rstrip())
+        if lines[i].rstrip().find('GROUP') == 4:
+            newrng = range(i, i+5)
+    for i in newrng:
+        print (lines[i].rstrip())
+print(get_logs(remote_conn))
