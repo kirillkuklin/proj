813441cc731e91f60892e007b370bbe4fa5eb118
Kirill Kuklin <kk90@mail.com>
2017-02-24 12:47:44 +0300

    get_logs has been renamed to get_online_redo_logs and returns now 3 lines of logs as desired

--- Oracle.py
+++ Oracle.py
@@ -30,9 +30,7 @@ def get_user(remote_conn):  # remote_conn type is <class 'paramiko.channel.Chann
     remote_conn.send("show user\n")
     time.sleep(2)
     output = remote_conn.recv(10000)
-    # return output
     sqlplus = output.decode("utf-8")
-    # return sqlplus.split('\n')
     for i in sqlplus.split('\n'):
         if i.find("USER") == 0:
             res[i.lstrip('SQL>').split()[0]] = i.lstrip('SQL>').split()[2][1:-1]
@@ -40,22 +38,22 @@ def get_user(remote_conn):  # remote_conn type is <class 'paramiko.channel.Chann
 
 # print(get_user(remote_conn))
 
-
-def get_logs(remote_conn):  # remote_conn type is <class 'paramiko.channel.Channel'>
+def get_online_redo_logs(remote_conn):  # remote_conn type is <class 'paramiko.channel.Channel'>
     remote_conn.send("sqlplus / as sysdba\n")
     time.sleep(1)
     remote_conn.send("set linesize 3000\nselect *from v$log;\n")
     time.sleep(2)
     output = remote_conn.recv(10000)
-    # return output
     sqlplus = output.decode("utf-8")
     lines = sqlplus.splitlines(True)
     rng = range (0, len(lines))
-    newrng = range (0, 1)
     for i in rng:
-        # print(i, lines[i].rstrip())
         if lines[i].rstrip().find('GROUP') == 4:
-            newrng = range(i, i+5)
-    for i in newrng:
-        print (lines[i].rstrip())
-print(get_logs(remote_conn))
+            res = lines[i:i+5]
+            for i in res:
+                value = (i.strip())
+                yield (value)
+
+for i in get_online_redo_logs(remote_conn): print(i)
+
+
